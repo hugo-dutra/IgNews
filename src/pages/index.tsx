d2541616d127c70e8829b79bf8c9e26539e96ded
hugo-dutra <hugo.dutra@hotmail.com>
2022-04-08 18:21:05 -0300

    First commit - project configurated

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return <>
+    <Head>
+      <title>Início | IgNews</title>
+    </Head>
+    <h1 >
+      Olá Mundo - <span>NextJs</span> com Typescript</h1>
+  </>
+
+}
+
+export default Home
